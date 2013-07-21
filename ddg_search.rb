0540bee6400b37767fa69677f2281e7dc1048844
Sean Redmond <github-smr@sneakemail.com>
2013-07-21 13:38:43 -0400

    Add non-iframe option

--- ddg_search.rb
+++ ddg_search.rb
@@ -8,7 +8,7 @@ module Jekyll
       @tag = tag_name
       @text = text
       @search_url = 'http://duckduckgo.com/search.html'
-      @valid_options = ['width', 'duck', 'site', 'prefill', 'bgcolor', 'focus']
+      @valid_options = ['width', 'duck', 'site', 'sites', 'prefill', 'bgcolor', 'focus']
       @options = get_options(text)
     end
  
@@ -25,10 +25,28 @@ module Jekyll
     #
     # Options with invalid keys or nil values are removed
     def get_options(opt_string)
-      parsed_opts = Hash[opt_string.split(/\s+/).map{|opt| opt.split(':')}].
-        delete_if{|k, v| (!@valid_options.include?(k)) || (v == nil) }.
-        map{|k,v| "#{k}=#{escape_options(k, v)}"}.join('&amp;')
+      parsed_opts = fixup_options(
+        Hash[opt_string.split(/\s+/).map{|opt| opt.split(':')}].
+        delete_if{|k, v| (!@valid_options.include?(k)) || (v == nil) }
+      )
     end
+
+    def fixup_options(opts)
+      # The iframe version uses the 'site' parameter, while the form version
+      # uses 'sites'. Conver one to the other where necessary
+      if @tag == 'ddg_search'
+        if opts['site'] == nil && opts['sites'] != nil
+          opts['site'] = opts.delete('sites')
+        end
+      else
+        if opts['sites'] == nil && opts['site'] != nil
+          opts['sites'] = opts.delete('site')
+        end
+      end
+
+      return opts
+    end
+
 
     # Escape query parameters, with special handling for prefill option
     #
@@ -48,15 +66,45 @@ module Jekyll
       return value
     end
 
-    def render(context)
-      url = [@search_url, @options].join('?')
+    def options_query(opts)
+      opts.map{|k,v| "#{k}=#{escape_options(k, v)}"}.join('&amp;')
+    end
+
+    def render_iframe(context)
+      url = [@search_url, options_query(@options)].join('?')
 <<HTML
 <iframe src="#{url}" 
         frameborder="0"></iframe>
 </iframe>
 HTML
     end
+
+    def form_inputs(opts)
+      opts.map{|k, v| 
+        %Q|<input type="hidden" name="#{k}" value="#{v}"/>|
+      }.join(' ')
+    end
+
+    def render_form(context)
+      inputs = form_inputs(@options)
+<<HTML
+<form method="get" id="search" action="http://duckduckgo.com/">
+  #{inputs}
+  <input type="text" name="q" maxlength="255" placeholder="Search"/>
+  <input type="submit" value="DuckDuckGo Search" style="visibility: hidden;" />
+</form>
+HTML
+    end
+
+    def render(context)
+      if @tag == 'ddg_search'
+        render_iframe(context)
+      else
+        render_form(context)
+      end
+    end
   end
 end
  
 Liquid::Template.register_tag('ddg_search', Jekyll::DdgSearchTag)
+Liquid::Template.register_tag('ddg_search_form', Jekyll::DdgSearchTag)
